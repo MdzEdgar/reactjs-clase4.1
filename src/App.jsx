c2db533c7380bd561d6b9daa6035fb1cdce705b5
Edgar Méndez <mdzredgar@gmail.com>
2023-02-07 21:04:29 -0600

    Metodo get finalizado.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import './App.css'
+import MovieCard from './components/MovieCard'
 
 function App() {
+  const [movies, setMovies] = useState([])
+
+  const URL_BASE = "https://movies-crud-2.academlo.tech/"
+
+  useEffect(() => {
+    axios.get(`${URL_BASE}movies/`)
+    .then((res) => setMovies(res.data))
+    .catch((err) => console.log(err))
+  }, [])
 
   return (
     <div className="App">
-     <h1>Movies</h1>
+      <h1>Movies App</h1>
+
+      <section>
+        {
+        movies.map((movie) => <MovieCard key={movie.key} movie={movie} />)
+        }
+      </section>
     </div>
-  )
+  );
 }
 
 export default App
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const MovieCard = ({movie}) => {
+  return (
+    <article>
+      <h3>{movie.name}</h3>
+      <ul>
+        <li><span>Genre: </span>{movie.genre}</li>
+        <li><span>Duration: </span>{movie.duration}</li>
+        <li><span>Release date:</span>{movie.release_date}</li>
+      </ul>
+    </article>
+  )
+}
+
+export default MovieCard
